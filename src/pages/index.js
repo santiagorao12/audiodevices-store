177348798c0278429dcea80a3b9bb55c6dedeceb
santiago <santiagorao43@gmail.com>
2023-02-22 19:22:02 +0800

    Deleted module.css and added content in index js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      hi 
-    </div>
+    <>
+      HeroBanner
+
+      <div className='products-Heading'>
+       <h2>Best Selling Products</h2>
+       <p>Speakers of many variations</p> 
+      </div> 
+
+      <div>
+        {['Product 1', 'Product 2'].map(
+          (product) => product)}
+      </div>
+
+      Footer
+    </>
   )
 }
 
