3bc16102ed1622011f4b6b0a804be08a615e51ef
santiago <santiagorao43@gmail.com>
2023-02-28 20:28:13 +0800

    Filled in Herobanner in components

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt ="headphones" 
+        className='hero-banner-image' />
+
+
+        <div>
+          <link href='/product/ID'>
+            <button type='button'>BUTTON TEXT</button>
+          </link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <h5>DESCRIPTION</h5>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
