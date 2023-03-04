2804d7a5b8b82c9fc1daf0145b39cf2f6029d061
santiago <santiagorao43@gmail.com>
2023-03-04 18:18:37 +0800

    to use images stored in sanity

--- LIB/client.js
+++ LIB/client.js
@@ -1,10 +1,18 @@
 import { SanityClient } from "@sanity/client";
 import { ImageUrlBuilder } from "next-sanity-image";
 
-const client = sanityClient({
+export const client = sanityClient({
     projectid: '1hwkywi2',
     dataset: 'production',
     apiVersion: '2022-04-04',
     useCdn: true,
     token: process.env.NEXT_PUBLIC_SANITY_TOKEN
-})
+});
+
+//to use the sanity images
+const builder = ImageUrlBuilder(client);
+
+
+//sanity giving access to url where it is stored
+export const urlFor = (source) => builder.image(source);
+
